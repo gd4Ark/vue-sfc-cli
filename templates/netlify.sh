1f5c7d7a4bc74903e5bc7f24787c244332df0513
levy <897895407@qq.com>
2020-07-14 17:18:33 +0800

    fix: exclude dev to master deploy preview

--- templates/netlify.sh
+++ templates/netlify.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "is netlify: $NETLIFY"
 echo "in branch: $BRANCH"
+echo "head: $HEAD"
 
 if [ "$NETLIFY" != "true" ]
 then
@@ -8,10 +9,9 @@ then
   exit 1
 fi
 
-if [ "$BRANCH" != "dev" ]
+if [ "$BRANCH" != "dev" ] && [ "$HEAD" != "dev" ]
 then
   yarn doc
 else
-  echo "this script only runs in dev branch, bye"
+  echo "this script only runs in targeting dev's PR deploy preview, bye"
 fi
-
